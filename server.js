34f0b4e349fa13c292bbc0aef5cc128f4cdde7f2
AbdElRhman-Sabry59 <abdo.sabry552002@gmail.com>
2026-08-09 00:50:37 +0300

    Fix MySQL SSL connection

--- server.js
+++ server.js
@@ -76,6 +76,10 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
+
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 // ==================================================
